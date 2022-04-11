9462075028105f93b6e5acf038c0cb2eefb8d2da
Nick Edwards <theadventuresofnickedwards@gmail.com>
2022-04-11 09:41:45 -0700

    deploying

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,19 +1,13 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/bookSearchEngineDB', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
+mongoose.connect(
+  // REFACTOR for Heroku deployment
+  // process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
+  process.env.MONGODB_URI || 'mongodb://localhost:27017/bookSearchEngineDB', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
 });
-
-// // ADDED: Connecting MongoDB to Heroku
-// mongoose.connect(
-//   process.env.MONGODB_URI || 'mongodb://localhost:27017/bookSearchEngineDB',
-//   {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//   },
-// );
 
 module.exports = mongoose.connection;
